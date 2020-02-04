65574957c5367935d57779239668b212944f3806
Santos Gonzalez <sgonzo3@gmail.com>
2020-02-04 15:51:54 -0500

    docs: clear commented code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css';
 
-
-// initial state with list of operators excluding '-' each time state is updated from initial, inlude '-' in lit of operators, when state is cleared or evaluate is done set operator in state to exclude '-' again
 function App() {
   return (
     <div className="App">
@@ -23,28 +21,14 @@ class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      // input: null,
       output: '',
       first: '',
       operator: '',
-      // heldMinus: false,
-      // second: 0,
       operatorsList: ["*", "/", "+"],
       hash: {
-        // "7": 7,
-        // "8": 8,
-        // "9": 9,
         "/": (first, second) => +first / +second,
-        // "4": 4,
-        // "5": 5,
-        // "6": 6,
         "*": (first, second) => +first * +second,
-        // "1": 1,
-        // "2": 2,
-        // "3": 3,
         "-": (first, second) => +first - +second,
-        // ".": ".",
-        // "0": 0,
         "=": (first, second) => console.log("="),
         "+": (first, second) => +first + +second,
       },
@@ -74,7 +58,6 @@ class Calculator extends React.Component {
 
   evaluate = () => {
     let answer = '0';
-    // console.log(this.state);
     if(this.state.first && this.state.operator){
       if(!this.state.hash[this.state.output]){
         answer = (this.state.operator(this.state.first, this.state.output));
@@ -82,14 +65,11 @@ class Calculator extends React.Component {
       }
     }
     return this.setState({
-      // input: answer,
       first: '' + answer,
       operator: '',
       operatorsList: ["*", "/", "+"],
-      //this.state.heldMinus ? '-' + answer : '' + answer,
       output: '' + answer,
-    },() => console.log(this.state))
-
+    });
   }
 
   checkDecimal(value){
@@ -98,7 +78,6 @@ class Calculator extends React.Component {
       return;
     }
     return this.setState({
-      // input: this.state.input + value,
       output: this.state.output + value,
       operatorsList: ["*", "/", "+"],
 
@@ -107,13 +86,12 @@ class Calculator extends React.Component {
 
   clear(){
     this.setState({
-         // input: [],
          output: '',
          first: '',
          second: '',
          operator: '',
          operatorsList: ["*", "/", "+"]
-        }, () => console.log(this.state))
+    });
   }
 
   checkOperator(value){
@@ -126,21 +104,16 @@ class Calculator extends React.Component {
       });
     } else {
       return this.setState({
-           // input: this.state.input + value,
-           // !this.state.hash[this.state.output] ? this.state.output : this.state.first
            first: this.state.output,
-          //  heldMinus: this.state.output === '-' ? true : false,
            output: '',
            operator: this.state.hash[value],
            operatorsList: ["*", "/", "+"]
-         }, () => console.log(this.state));
+         });
     }
   }
 
   handleClick = ({target}) => {
-    console.log(this.state)
     const {value} = target;
-    // const operatorsList = ["*", "/", "+"];
     if(value === ".") {
       return this.checkDecimal(value);
     } else if(value === "0" && this.state.output === "") {
@@ -152,22 +125,11 @@ class Calculator extends React.Component {
     } else if(this.state.operatorsList.includes(value)) {
       return this.checkOperator(value);
     } 
-    // else if(value === "-"){
-    //   if(this.state.output === "0"){
-    //     this.setState({
-    //       // input: this.state.input + value,
-    //       // didnt work: (value === '-' ? this.state.output : value )
-    //       operatorsList: ["*", "/", "+", "-"],
-    //       output:  value,
-    //     }, () => console.log(this.state));
-    //   } else {return this.checkOperator(value)}
-    // } 
     else {
         this.setState({
-             // input: this.state.input + value,
              operatorsList: ["*", "/", "+", "-"],
              output: this.state.output + value,
-           }, () => console.log(this.state));
+           });
     }
   }
 
